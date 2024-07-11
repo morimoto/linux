c1c6dcf4dbfeac37894e5f70c236dbde29e87d6e
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2024-07-11 10:09:16 +0900

    msiof
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/sh/msiof.c
+++ sound/soc/sh/msiof.c
@@ -6,11 +6,15 @@
 // Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
 //
 // Based on fsi.c
-// Copyright (c) 2009 Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
+// Copyright (c) 2009 Kuninori Morimoto <morimoto.kuninori@renesas.com>
 
 #include <linux/module.h>
-#include <linux/platform_device.h>
 #include <linux/of.h>
+#include <linux/of_dma.h>
+#include <linux/platform_device.h>
+#include <linux/pm_runtime.h>
+#include <sound/dmaengine_pcm.h>
+#include <sound/soc.h>
 
 /* register */
 #define SITMDR1		0000
@@ -27,47 +31,37 @@
 #define SITFDR		0050
 #define SIRFDR		0060
 
+/* spec */
+#define MSIOF_RATES	SNDRV_PCM_RATE_8000_192000
+#define MSIOF_FMTS	(SNDRV_PCM_FMTBIT_S16_LE |\
+			 SNDRV_PCM_FMTBIT_S24_LE)
+
 struct msiof_priv {
 	struct platform_device *pdev;
 	spinlock_t lock;
 	void __iomem *base;
+	struct dma_chan *dma_chan[SNDRV_PCM_STREAM_LAST + 1];
 };
 
-static snd_pcm_uframes_t msiof_pointer(struct snd_soc_component *component,
-				       struct snd_pcm_substream *substream)
+static int msiof_is_play(struct snd_pcm_substream *substream)
 {
-	struct snd_pcm_runtime *runtime = rsnd_io_to_runtime(io);
-	struct rsnd_dma *dma = rsnd_mod_to_dma(mod);
-	struct rsnd_dmaen *dmaen = rsnd_dma_to_dmaen(dma);
-	struct dma_tx_state state;
-	enum dma_status status;
-	unsigned int pos = 0;
-
-	status = dmaengine_tx_status(dmaen->chan, dmaen->cookie, &state);
-	if (status == DMA_IN_PROGRESS || status == DMA_PAUSED) {
-		if (state.residue > 0 && state.residue <= dmaen->dma_len)
-			pos = dmaen->dma_len - state.residue;
-	}
-	*pointer = bytes_to_frames(runtime, pos);
-
-	return 0;
+	return substream->stream == SNDRV_PCM_STREAM_PLAYBACK;
 }
 
-#define PREALLOC_BUFFER		(32 * 1024)
-#define PREALLOC_BUFFER_MAX	(32 * 1024)
-static int msiof_pcm_new(struct snd_soc_component *component,
-			 struct snd_soc_pcm_runtime *rtd)
-{
-	snd_pcm_set_managed_buffer_all(
-		rtd->pcm,
-		SNDRV_DMA_TYPE_DEV,
-		rtd->card->snd_card->dev,
-		PREALLOC_BUFFER, PREALLOC_BUFFER_MAX);
-	return 0;
-}
+static const struct snd_soc_dai_ops msiof_dai_ops = {
+/*
+	.startup	= fsi_dai_startup,
+	.shutdown	= fsi_dai_shutdown,
+	.trigger	= fsi_dai_trigger,
+	.set_fmt	= fsi_dai_set_fmt,
+	.hw_params	= fsi_dai_hw_params,
+	.auto_selectable_formats	= &fsi_dai_formats,
+	.num_auto_selectable_formats	= 1,
+*/
+};
 
-static struct snd_soc_dai_driver msiof_dai = {
-	.name			= "msiof-dai",
+static struct snd_soc_dai_driver msiof_dai_driver = {
+	.name = "msiof-dai",
 	.playback = {
 		.rates		= MSIOF_RATES,
 		.formats	= MSIOF_FMTS,
@@ -83,12 +77,65 @@ static struct snd_soc_dai_driver msiof_dai = {
 	.ops = &msiof_dai_ops,
 };
 
-static const struct snd_soc_component_driver msiof_component = {
-	.name		= "msiof",
-	.open		= msiof_pcm_open,
-	.pointer	= msiof_pointer,
-	.pcm_construct	= msiof_pcm_new,
+static int msiof_open(struct snd_soc_component *component,
+		      struct snd_pcm_substream *substream)
+{
+	struct device *dev = component->dev;
+	struct msiof_priv *priv = dev_get_drvdata(dev);
+	struct dma_chan *chan;
+	int is_play = msiof_is_play(substream);
+	char *dma_names[] = {"rx", "tx"};
+
+	chan = of_dma_request_slave_channel(dev->of_node, dma_names[is_play]);
+
+	if (IS_ERR_OR_NULL(chan))
+		return PTR_ERR(chan);
+
+	priv->dma_chan[is_play] = chan;
+
+	return snd_dmaengine_pcm_open(substream, chan);
+}
+
+static int msiof_close(struct snd_soc_component *component,
+		       struct snd_pcm_substream *substream)
+{
+	struct msiof_priv *priv = dev_get_drvdata(component->dev);
+	int is_play = msiof_is_play(substream);
+
+	priv->dma_chan[is_play] = NULL;
+
+	return snd_dmaengine_pcm_close_release_chan(substream);
+}
+
+static snd_pcm_uframes_t msiof_pointer(struct snd_soc_component *component,
+				       struct snd_pcm_substream *substream)
+{
+	return snd_dmaengine_pcm_pointer(substream);
+}
+
+#define PREALLOC_BUFFER		(32 * 1024)
+#define PREALLOC_BUFFER_MAX	(32 * 1024)
+static int msiof_new(struct snd_soc_component *component,
+		     struct snd_soc_pcm_runtime *rtd)
+{
+	snd_pcm_set_managed_buffer_all(rtd->pcm, SNDRV_DMA_TYPE_DEV,
+				       rtd->card->snd_card->dev,
+				       PREALLOC_BUFFER, PREALLOC_BUFFER_MAX);
+	return 0;
+}
+
+static const struct snd_soc_component_driver msiof_component_driver = {
+	.name			= "msiof",
+	.open			= msiof_open,
+	.close			= msiof_close,
+	.pointer		= msiof_pointer,
+	.pcm_construct		= msiof_new,
 };
+
+static irqreturn_t msiof_interrupt(int irq, void *data)
+{
+	return IRQ_HANDLED;
+}
 
 static int msiof_probe(struct platform_device *pdev)
 {
@@ -121,24 +168,12 @@ static int msiof_probe(struct platform_device *pdev)
 	spin_lock_init(&priv->lock);
 	platform_set_drvdata(pdev, priv);
 
-	pm_runtime_enable(dev);
+	devm_pm_runtime_enable(dev);
 
-	ret = devm_snd_soc_register_component(dev, &msiof_component,
-					      msiof_dai, ARRAY_SIZE(msiof_dai));
-	if (ret < 0) {
-		dev_err(&pdev->dev, "cannot snd component register\n");
-		goto exit_fsib;
-	}
+	ret = devm_snd_soc_register_component(dev, &msiof_component_driver,
+					      &msiof_dai_driver, 1);
 
 	return ret;
-
-
-	return 0;
-}
-
-static void msiof_remove(struct platform_device *pdev)
-{
-	
 }
 
 static const struct of_device_id msiof_of_match[] = {
@@ -154,7 +189,7 @@ static struct platform_driver msiof_driver = {
 		.of_match_table = msiof_of_match,
 	},
 	.probe		= msiof_probe,
-	.remove_new	= msiof_remove,
+//	.remove_new	= msiof_remove,
 };
 module_platform_driver(msiof_driver);
 
