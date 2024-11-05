a93487b9663e5a7ddec5e02e6a4eb733d24728d4
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2024-11-05 15:28:27 +0900

    ASoC: codecs: twl4030: remove snd_soc_dai_set_tristate()
    snd_soc_dai_set_tristate() has never been used before.
    Let's remove it.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/codecs/twl4030.c
+++ sound/soc/codecs/twl4030.c
@@ -1891,19 +1891,6 @@ static int twl4030_set_dai_fmt(struct snd_soc_dai *codec_dai, unsigned int fmt)
 	return 0;
 }
 
-static int twl4030_set_tristate(struct snd_soc_dai *dai, int tristate)
-{
-	struct snd_soc_component *component = dai->component;
-	u8 reg = twl4030_read(component, TWL4030_REG_AUDIO_IF);
-
-	if (tristate)
-		reg |= TWL4030_AIF_TRI_EN;
-	else
-		reg &= ~TWL4030_AIF_TRI_EN;
-
-	return twl4030_write(component, TWL4030_REG_AUDIO_IF, reg);
-}
-
 /* In case of voice mode, the RX1 L(VRX) for downlink and the TX2 L/R
  * (VTXL, VTXR) for uplink has to be enabled/disabled. */
 static void twl4030_voice_enable(struct snd_soc_component *component, int direction,
@@ -2085,19 +2072,6 @@ static int twl4030_voice_set_dai_fmt(struct snd_soc_dai *codec_dai,
 	return 0;
 }
 
-static int twl4030_voice_set_tristate(struct snd_soc_dai *dai, int tristate)
-{
-	struct snd_soc_component *component = dai->component;
-	u8 reg = twl4030_read(component, TWL4030_REG_VOICE_IF);
-
-	if (tristate)
-		reg |= TWL4030_VIF_TRI_EN;
-	else
-		reg &= ~TWL4030_VIF_TRI_EN;
-
-	return twl4030_write(component, TWL4030_REG_VOICE_IF, reg);
-}
-
 #define TWL4030_RATES	 (SNDRV_PCM_RATE_8000_48000)
 #define TWL4030_FORMATS	 (SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S32_LE)
 
@@ -2107,7 +2081,6 @@ static const struct snd_soc_dai_ops twl4030_dai_hifi_ops = {
 	.hw_params	= twl4030_hw_params,
 	.set_sysclk	= twl4030_set_dai_sysclk,
 	.set_fmt	= twl4030_set_dai_fmt,
-	.set_tristate	= twl4030_set_tristate,
 };
 
 static const struct snd_soc_dai_ops twl4030_dai_voice_ops = {
@@ -2116,7 +2089,6 @@ static const struct snd_soc_dai_ops twl4030_dai_voice_ops = {
 	.hw_params	= twl4030_voice_hw_params,
 	.set_sysclk	= twl4030_voice_set_dai_sysclk,
 	.set_fmt	= twl4030_voice_set_dai_fmt,
-	.set_tristate	= twl4030_voice_set_tristate,
 };
 
 static struct snd_soc_dai_driver twl4030_dai[] = {
