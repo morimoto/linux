d322aac25d99d77b9c9f5121b349a89d4a00be63
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2025-03-04 10:28:29 +0900

    ASoC: pxa: use string choices helper
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/pxa/pxa-ssp.c
+++ sound/soc/pxa/pxa-ssp.c
@@ -88,8 +88,7 @@ static int pxa_ssp_startup(struct snd_pcm_substream *substream,
 	dma = kzalloc(sizeof(struct snd_dmaengine_dai_dma_data), GFP_KERNEL);
 	if (!dma)
 		return -ENOMEM;
-	dma->chan_name = substream->stream == SNDRV_PCM_STREAM_PLAYBACK ?
-		"tx" : "rx";
+	dma->chan_name = str_tx_rx(substream->stream == SNDRV_PCM_STREAM_PLAYBACK);
 
 	snd_soc_dai_set_dma_data(cpu_dai, substream, dma);
 
