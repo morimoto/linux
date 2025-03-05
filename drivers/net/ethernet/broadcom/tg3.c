c8d9c0313a7889c328d0023fc398c85ca6859c6b
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2025-03-05 16:09:01 +0900

    net: ethernet: broadcom: tg3: use string choices helper
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- drivers/net/ethernet/broadcom/tg3.c
+++ drivers/net/ethernet/broadcom/tg3.c
@@ -1883,14 +1883,12 @@ static void tg3_link_report(struct tg3 *tp)
 			     "full" : "half"));
 
 		netdev_info(tp->dev, "Flow control is %s for TX and %s for RX\n",
-			    (tp->link_config.active_flowctrl & FLOW_CTRL_TX) ?
-			    "on" : "off",
-			    (tp->link_config.active_flowctrl & FLOW_CTRL_RX) ?
-			    "on" : "off");
+			    str_on_off(tp->link_config.active_flowctrl & FLOW_CTRL_TX),
+			    str_on_off(tp->link_config.active_flowctrl & FLOW_CTRL_RX));
 
 		if (tp->phy_flags & TG3_PHYFLG_EEE_CAP)
 			netdev_info(tp->dev, "EEE is %s\n",
-				    tp->setlpicnt ? "enabled" : "disabled");
+				    str_enabled_disabled(tp->setlpicnt));
 
 		tg3_ump_link_report(tp);
 	}
