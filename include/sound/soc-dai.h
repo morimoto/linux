fe332ca9a11adf092d9a62b7dc5f651d9860e574
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-02-13 13:38:29 +0900

    ASoC: soc-dai: add snd_soc_dai_id()
    DAI parameter will be capsuled soon, will be not enable to access from
    each drivers. add snd_soc_dai_id() to get ID from DAI.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- include/sound/soc-dai.h
+++ include/sound/soc-dai.h
@@ -283,6 +283,7 @@ int snd_soc_dai_matches_args(const struct snd_soc_dai *dai,
 int snd_soc_dai_matches_dlc(struct snd_soc_dai *dai,
 			    const struct snd_soc_dai_link_component *dlc);
 const char *snd_soc_dai_name(const struct snd_soc_dai *dai);
+int snd_soc_dai_id(const struct snd_soc_dai *dai);
 
 struct snd_soc_dai_ops {
 	/* DAI driver callbacks */
