49305ae96cc6c131ba346a5058720a68c2437395
Geert Uytterhoeven <geert+renesas@glider.be>
2026-02-16 16:06:11 +0100

    firmware: arm_scmi: quirk: Handle bad power domains on R-Car X5H
    Renesas R-Car X5H SCP FW SDKv4.28.0 - v4.36.0 advertise a
    few power domains that crash the system when touched or powered off,
    and that do not support the POWER_STATE_GET command.
    
    Add a quirk to prevent such crashes, and to ignore the POWER_STATE_GET
    command.
    
    As the SCMI power domain IDs are identical for all versions, the
    quirk can be shared.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/firmware/arm_scmi/quirks.h
+++ drivers/firmware/arm_scmi/quirks.h
@@ -50,5 +50,6 @@ DECLARE_SCMI_QUIRK(clock_rates_triplet_out_of_spec);
 DECLARE_SCMI_QUIRK(clock_rcar_x5h_4_28_crit_clocks);
 DECLARE_SCMI_QUIRK(clock_rcar_x5h_4_31_crit_clocks);
 DECLARE_SCMI_QUIRK(perf_level_get_fc_force);
+DECLARE_SCMI_QUIRK(power_rcar_x5h_4_28_bad_domains);
 
 #endif /* _SCMI_QUIRKS_H */
