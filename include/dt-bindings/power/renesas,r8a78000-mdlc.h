60c073987a00a1a542bf4a48ac752347a1f5b6d2
Geert Uytterhoeven <geert+renesas@glider.be>
2026-02-19 11:36:17 +0100

    dt-bindings: power: Document Renesas R-Car X5H Module Controller
    Document support for Renesas R-Car X5H Module Controllers, and
    add definitions for power domains not backed by registers.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

---
+++ include/dt-bindings/power/renesas,r8a78000-mdlc.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (C) 2026 Glider bv
+ */
+#ifndef __DT_BINDINGS_POWER_RENESAS_R8A78000_MDLC_H__
+#define __DT_BINDINGS_POWER_RENESAS_R8A78000_MDLC_H__
+
+/* R-Car X5H MDLC Power Domains */
+
+#define R8A78000_MDLC_PD_AON			0x40
+#define R8A78000_MDLC_PD_SCP			0x41
+#define R8A78000_MDLC_PD_APL			0x42
+#define R8A78000_MDLC_PD_CMN			0x43
+#define R8A78000_MDLC_PD_ACL			0x44
+
+#endif /* __DT_BINDINGS_POWER_RENESAS_R8A78000_MDLC_H__ */
