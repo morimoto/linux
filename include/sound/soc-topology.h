00354963167ab07ed1e0119ad69b33d277bad0a3
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-03-25 13:51:27 +0900

    ASoC: soc-dapm: rename list to dobj_list
    Current DAPM is using "list" to connecting to "donj_list".
    
    It is unclear and confusable naming. Let's rename it as
    "dobj_list" and "dobj_list_head".
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- include/sound/soc-topology.h
+++ include/sound/soc-topology.h
@@ -61,7 +61,7 @@ struct snd_soc_dobj_widget {
 struct snd_soc_dobj {
 	enum snd_soc_dobj_type type;
 	unsigned int index;	/* objects can belong in different groups */
-	struct list_head list;
+	struct list_head dobj_list;
 	int (*unload)(struct snd_soc_component *comp, struct snd_soc_dobj *dobj);
 	union {
 		struct snd_soc_dobj_control control;
