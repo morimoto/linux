3a9242b7bd594d28affb66d076405a6c5e36e24d
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-03-26 10:52:26 +0900

    ASoC: soc-card: move snd_soc_card_dapm_mutex_xxx() to soc-card.c
    struct snd_soc_card will be capsuled soon, its member will not be
    able to access from non soc-card.c.
    
    Move snd_soc_card_dapm_mutex_xxx() to soc-card.c
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- include/sound/soc-card.h
+++ include/sound/soc-card.h
@@ -111,4 +111,9 @@ struct snd_soc_dai *snd_soc_card_get_codec_dai(struct snd_soc_card *card,
 	return NULL;
 }
 
+void snd_soc_card_dapm_mutex_lock_root(struct snd_soc_card *card);
+void snd_soc_card_dapm_mutex_lock(struct snd_soc_card *card);
+void snd_soc_card_dapm_mutex_unlock(struct snd_soc_card *card);
+void snd_soc_card_dapm_mutex_assert_held(struct snd_soc_card *card);
+
 #endif /* __SOC_CARD_H */
