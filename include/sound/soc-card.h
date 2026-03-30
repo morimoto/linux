b2b954be39037f597ecffed2a9ca55200b013b63
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-03-30 11:53:57 +0900

    ASoC: soc-card: move for_each_card_xxx() to soc-card.h
    struct snd_soc_card will be capsuled soon, its member will not be
    able to access from non soc-card.c.
    
    Move for_each_card_xxx() to soc-card.h
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- include/sound/soc-card.h
+++ include/sound/soc-card.h
@@ -94,6 +94,37 @@ void snd_soc_card_set_priv(struct snd_soc_card *card, void *data);
 struct snd_soc_pcm_runtime *snd_soc_card_to_rtd(struct snd_soc_card *card,
 						 struct snd_soc_dai_link *dai_link);
 
+#define for_each_card_prelinks(card, i, link)				\
+	for ((i) = 0;							\
+	     ((i) < (card)->num_links) && ((link) = &(card)->dai_link[i]); \
+	     (i)++)
+#define for_each_card_pre_auxs(card, i, aux)				\
+	for ((i) = 0;							\
+	     ((i) < (card)->num_aux_devs) && ((aux) = &(card)->aux_dev[i]); \
+	     (i)++)
+
+#define for_each_card_rtds(card, rtd)					\
+	list_for_each_entry(rtd, &(card)->rtd_list_head, rtd_list)
+#define for_each_card_rtds_safe(card, rtd, _rtd)			\
+	list_for_each_entry_safe(rtd, _rtd, &(card)->rtd_list_head, rtd_list)
+
+#define for_each_card_auxs(card, component)				\
+	list_for_each_entry(component, &card->aux_list_head, aux_list)
+#define for_each_card_auxs_safe(card, component, _comp)			\
+	list_for_each_entry_safe(component, _comp,			\
+				 &card->aux_list_head, aux_list)
+
+#define for_each_card_components(card, component)			\
+	list_for_each_entry(component, &(card)->component_list_head, component_list)
+
+#define for_each_card_dapms(card, dapm)					\
+	list_for_each_entry(dapm, &card->dapm_list_head, dapm_list)
+
+#define for_each_card_widgets(card, w)					\
+	list_for_each_entry(w, &card->widget_list_head, widget_list)
+#define for_each_card_widgets_safe(card, w, _w)				\
+	list_for_each_entry_safe(w, _w, &card->widget_list_head, widget_list)
+
 /* REMOVE ME */
 #define snd_soc_card_set_drvdata	snd_soc_card_set_priv
 #define snd_soc_card_get_drvdata	snd_soc_card_to_priv
