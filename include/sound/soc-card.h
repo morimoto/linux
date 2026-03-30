8f16f3161d57e59d4b1b4123f2694e5d5a1ba81a
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-03-30 15:22:30 +0900

    ASoC: soc-card: tidyup for_each_card_dapms()
    struct snd_soc_card will be capsuled soon, its member will not be
    able to access from non soc-card.c.
    
    Don't use card->xxx_list_head
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- include/sound/soc-card.h
+++ include/sound/soc-card.h
@@ -260,7 +260,7 @@ SOC_CARD_LIST_HEAD_DEFINE(dapm_dirty);
 	     component = snd_soc_component_from_component_list(snd_soc_component_to_component_list(component)->next))
 
 #define for_each_card_dapms(card, dapm)					\
-	list_for_each_entry(dapm, &card->dapm_list_head, dapm_list)
+	list_for_each_entry(dapm, snd_soc_card_to_dapm_list_head(card), dapm_list)
 
 #define for_each_card_widgets(card, w)					\
 	list_for_each_entry(w, &card->widget_list_head, widget_list)
