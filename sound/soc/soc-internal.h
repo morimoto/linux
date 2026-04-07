ab4280bc12907989bdfd390e35a3117eefa13d5f
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-04-08 08:54:37 +0900

    ASoC: soc-card: add snd_soc_card_deferred_resume()
    struct snd_soc_card will be capsuled soon, its member will not be
    able to access from non soc-card.c.
    
    Adds snd_soc_card_deferred_resume()
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/soc-internal.h
+++ sound/soc/soc-internal.h
@@ -44,6 +44,9 @@ void snd_soc_card_dpcm_mutex_lock(struct snd_soc_card *card);
 void snd_soc_card_dpcm_mutex_unlock(struct snd_soc_card *card);
 void snd_soc_card_dpcm_mutex_assert_held(struct snd_soc_card *card);
 void snd_soc_card_flush_all_delayed_work(struct snd_soc_card *card);
+#ifdef CONFIG_PM_SLEEP
+int snd_soc_card_deferred_resume(struct snd_soc_card *card);
+#endif
 
 /*
  * In soc-dai
