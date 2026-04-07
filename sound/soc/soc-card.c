20f105d6997849aa51d1c7bd8629d5b39a97af0c
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-04-08 08:47:14 +0900

    ASoC: soc-card: add snd_soc_card_connect_rtd()
    struct snd_soc_card will be capsuled soon, its member will not be
    able to access from non soc-card.c.
    
    rtd will be connected to card. We need to have such function.
    Adds snd_soc_card_connect_rtd() for it.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/soc-card.c
+++ sound/soc/soc-card.c
@@ -60,6 +60,13 @@ struct snd_soc_pcm_runtime *snd_soc_card_to_rtd(struct snd_soc_card *card,
 }
 EXPORT_SYMBOL_GPL(snd_soc_card_to_rtd);
 
+int snd_soc_card_connect_rtd(struct snd_soc_card *card, struct snd_soc_pcm_runtime *rtd)
+{
+	list_add_tail(&rtd->rtd_list, &card->rtd_list_head);
+
+	return card->num_rtd++; /* return total number of rtd */
+}
+
 #ifdef CONFIG_DEBUG_FS
 static void snd_soc_card_debugfs_init(struct snd_soc_card *card)
 {
