46df424e178bf5594aeaeecc0beb64aa82da50c1
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-04-20 13:56:31 +0900

    ASoC: atmel: atmel-pdmic: use snd_soc_card_register()
    struct snd_soc_card will be capsuled soon, its member will not be
    able to access from non soc-card.c.
    
    func snd_soc_register_card() will be removed soon.
    Use  snd_soc_card_register() instead.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/atmel/atmel-pdmic.c
+++ sound/soc/atmel/atmel-pdmic.c
@@ -485,12 +485,13 @@ static const struct snd_soc_component_driver atmel_pdmic_cpu_dai_component = {
 };
 
 /* ASoC sound card */
-static int atmel_pdmic_asoc_card_init(struct device *dev,
-				struct snd_soc_card *card)
+static int atmel_pdmic_asoc_card_init(struct snd_soc_card *card,
+				      struct snd_soc_card_driver *card_driver)
 {
 	struct snd_soc_dai_link *dai_link;
 	struct atmel_pdmic *dd = snd_soc_card_get_drvdata(card);
 	struct snd_soc_dai_link_component *comp;
+	struct device *dev = snd_soc_card_to_dev(card);
 
 	dai_link = devm_kzalloc(dev, sizeof(*dai_link), GFP_KERNEL);
 	if (!dai_link)
@@ -510,10 +511,9 @@ static int atmel_pdmic_asoc_card_init(struct device *dev,
 	dai_link->stream_name		= "PDMIC PCM";
 	dai_link->cpus->dai_name	= dev_name(dev);
 
-	card->dai_link	= dai_link;
-	card->num_links	= 1;
-	card->name	= dd->pdata->card_name;
-	card->dev	= dev;
+	card_driver->dai_link		= dai_link;
+	card_driver->num_links		= 1;
+	snd_soc_card_set_name(card, dd->pdata->card_name);
 
 	return 0;
 }
@@ -574,6 +574,7 @@ static int atmel_pdmic_probe(struct platform_device *pdev)
 	void __iomem *io_base;
 	const struct atmel_pdmic_pdata *pdata;
 	struct snd_soc_card *card;
+	struct snd_soc_card_driver *card_driver;
 	unsigned int rate_min, rate_max;
 	int ret;
 
@@ -661,21 +662,22 @@ static int atmel_pdmic_probe(struct platform_device *pdev)
 	}
 
 	/* register sound card */
-	card = devm_kzalloc(dev, sizeof(*card), GFP_KERNEL);
-	if (!card) {
+	card = snd_soc_card_alloc(dev);
+	card_driver = devm_kzalloc(dev, sizeof(*card_driver), GFP_KERNEL);
+	if (!card || !card_driver) {
 		ret = -ENOMEM;
 		goto unregister_codec;
 	}
 
 	snd_soc_card_set_drvdata(card, dd);
 
-	ret = atmel_pdmic_asoc_card_init(dev, card);
+	ret = atmel_pdmic_asoc_card_init(card, card_driver);
 	if (ret) {
 		dev_err(dev, "failed to init sound card: %d\n", ret);
 		goto unregister_codec;
 	}
 
-	ret = devm_snd_soc_register_card(dev, card);
+	ret = devm_snd_soc_card_register(card, card_driver);
 	if (ret) {
 		dev_err(dev, "failed to register sound card: %d\n", ret);
 		goto unregister_codec;
