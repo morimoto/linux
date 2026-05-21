5a6eb5b2acc8a3f63b4138e53b6dc3b1b89b640e
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-05-21 14:01:37 +0900

    ASoC: intel: avs: rt298: use snd_soc_card_register()
    struct snd_soc_card will be capsuled soon, its member will not be
    able to access from non soc-card.c.
    
    func snd_soc_register_card() will be removed soon.
    Use  snd_soc_card_register() instead.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/intel/avs/boards/rt298.c
+++ sound/soc/intel/avs/boards/rt298.c
@@ -200,6 +200,7 @@ static int avs_rt298_probe(struct platform_device *pdev)
 	struct snd_soc_acpi_mach *mach;
 	struct avs_mach_pdata *pdata;
 	struct snd_soc_card *card;
+	struct snd_soc_card_driver *card_driver;
 	struct snd_soc_jack *jack;
 	struct device *dev = &pdev->dev;
 	int ssp_port, tdm_slot, ret;
@@ -218,32 +219,33 @@ static int avs_rt298_probe(struct platform_device *pdev)
 	}
 
 	jack = devm_kzalloc(dev, sizeof(*jack), GFP_KERNEL);
-	card = devm_kzalloc(dev, sizeof(*card), GFP_KERNEL);
-	if (!jack || !card)
+	card = snd_soc_card_alloc(dev);
+	card_driver = devm_kzalloc(dev, sizeof(*card_driver), GFP_KERNEL);
+	if (!jack || !card || !card_driver)
 		return -ENOMEM;
 
 	if (pdata->obsolete_card_names) {
-		card->name = "avs_rt298";
+		snd_soc_card_set_name(card, "avs_rt298");
 	} else {
-		card->driver_name = "avs_rt298";
-		card->long_name = card->name = "AVS I2S ALC298";
+		card_driver->driver_name = "avs_rt298";
+		snd_soc_card_set_name(card, "AVS I2S ALC298");
+		snd_soc_card_set_long_name(card, "AVS I2S ALC298");
 	}
-	card->dev = dev;
-	card->owner = THIS_MODULE;
-	card->suspend_pre = avs_card_suspend_pre;
-	card->resume_post = avs_card_resume_post;
-	card->dai_link = dai_link;
-	card->num_links = 1;
-	card->controls = card_controls;
-	card->num_controls = ARRAY_SIZE(card_controls);
-	card->dapm_widgets = card_widgets;
-	card->num_dapm_widgets = ARRAY_SIZE(card_widgets);
-	card->dapm_routes = card_base_routes;
-	card->num_dapm_routes = ARRAY_SIZE(card_base_routes);
-	card->fully_routed = true;
+	card_driver->owner = THIS_MODULE;
+	card_driver->suspend_pre = avs_card_suspend_pre;
+	card_driver->resume_post = avs_card_resume_post;
+	card_driver->dai_link = dai_link;
+	card_driver->num_links = 1;
+	card_driver->controls = card_controls;
+	card_driver->num_controls = ARRAY_SIZE(card_controls);
+	card_driver->dapm_widgets = card_widgets;
+	card_driver->num_dapm_widgets = ARRAY_SIZE(card_widgets);
+	card_driver->dapm_routes = card_base_routes;
+	card_driver->num_dapm_routes = ARRAY_SIZE(card_base_routes);
+	card_driver->fully_routed = true;
 	snd_soc_card_set_drvdata(card, jack);
 
-	return devm_snd_soc_register_deferrable_card(dev, card);
+	return devm_snd_soc_card_register(card, card_driver);
 }
 
 static const struct platform_device_id avs_rt298_driver_ids[] = {
