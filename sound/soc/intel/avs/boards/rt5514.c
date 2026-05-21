1c5d612ea278c39570ea86b1cb6d996d21cc74bd
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-05-21 14:02:53 +0900

    ASoC: intel: avs: rt5514: use snd_soc_card_register()
    struct snd_soc_card will be capsuled soon, its member will not be
    able to access from non soc-card.c.
    
    func snd_soc_register_card() will be removed soon.
    Use  snd_soc_card_register() instead.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/intel/avs/boards/rt5514.c
+++ sound/soc/intel/avs/boards/rt5514.c
@@ -135,6 +135,7 @@ static int avs_rt5514_probe(struct platform_device *pdev)
 	struct snd_soc_acpi_mach *mach;
 	struct avs_mach_pdata *pdata;
 	struct snd_soc_card *card;
+	struct snd_soc_card_driver *card_driver;
 	struct device *dev = &pdev->dev;
 	int ssp_port, tdm_slot, ret;
 
@@ -151,27 +152,28 @@ static int avs_rt5514_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	card = devm_kzalloc(dev, sizeof(*card), GFP_KERNEL);
-	if (!card)
+	card = snd_soc_card_alloc(dev);
+	card_driver = devm_kzalloc(dev, sizeof(*card_driver), GFP_KERNEL);
+	if (!card || !card_driver)
 		return -ENOMEM;
 
 	if (pdata->obsolete_card_names) {
-		card->name = "avs_rt5514";
+		snd_soc_card_set_name(card, "avs_rt5514");
 	} else {
-		card->driver_name = "avs_rt5514";
-		card->long_name = card->name = "AVS I2S ALC5514";
+		card_driver->driver_name = "avs_rt5514";
+		snd_soc_card_set_name(card, "AVS I2S ALC5514");
+		snd_soc_card_set_long_name(card, "AVS I2S ALC5514");
 	}
-	card->dev = dev;
-	card->owner = THIS_MODULE;
-	card->dai_link = dai_link;
-	card->num_links = 1;
-	card->dapm_widgets = card_widgets;
-	card->num_dapm_widgets = ARRAY_SIZE(card_widgets);
-	card->dapm_routes = card_base_routes;
-	card->num_dapm_routes = ARRAY_SIZE(card_base_routes);
-	card->fully_routed = true;
+	card_driver->owner = THIS_MODULE;
+	card_driver->dai_link = dai_link;
+	card_driver->num_links = 1;
+	card_driver->dapm_widgets = card_widgets;
+	card_driver->num_dapm_widgets = ARRAY_SIZE(card_widgets);
+	card_driver->dapm_routes = card_base_routes;
+	card_driver->num_dapm_routes = ARRAY_SIZE(card_base_routes);
+	card_driver->fully_routed = true;
 
-	return devm_snd_soc_register_deferrable_card(dev, card);
+	return devm_snd_soc_card_register(card, card_driver);
 }
 
 static const struct platform_device_id avs_rt5514_driver_ids[] = {
