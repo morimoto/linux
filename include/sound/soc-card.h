a6aba92d378adff641c654d395efa0e0bd316ba5
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-05-27 12:12:04 +0900

    ASoC: soc-card: move snd_soc_of_parse_aux_devs() to soc-card.c
    struct snd_soc_card will be capsuled soon, its member will not be
    able to access from non soc-card.c.
    
    Move snd_soc_of_parse_aux_devs() to soc-card.c
    
    It renames function name
    	- snd_soc_of_parse_aux_devs()
    	+ snd_soc_card_of_parse_aux_devs()
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- include/sound/soc-card.h
+++ include/sound/soc-card.h
@@ -52,6 +52,7 @@ int snd_soc_card_of_parse_simple_widgets(struct snd_soc_card *card, const char *
 int snd_soc_card_of_parse_name(struct snd_soc_card *card, const char *propname);
 int snd_soc_card_of_parse_pin_switches(struct snd_soc_card *card, const char *propname);
 int snd_soc_card_of_parse_audio_routing(struct snd_soc_card *card, const char *propname);
+int snd_soc_card_of_parse_aux_devs(struct snd_soc_card *card, const char *propname);
 
 #ifdef CONFIG_PCI
 void snd_soc_card_set_pci_ssid(struct snd_soc_card *card,
