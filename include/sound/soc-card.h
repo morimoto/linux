1c215d91c7dea7e34418b0236625a146cd6deb95
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-06-04 13:55:44 +0900

    ASoC: soc-card: move snd_soc_fixup_dai_links_platform_name() to soc-card.c
    struct snd_soc_card will be capsuled soon, its member will not be
    able to access from non soc-card.c.
    
    Move snd_soc_fixup_dai_links_platform_name() to soc-card.c
    
    It renames function name
    	- snd_soc_fixup_dai_links_platform_name()
    	+ snd_soc_card_fixup_dai_links_platform_name()
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- include/sound/soc-card.h
+++ include/sound/soc-card.h
@@ -55,6 +55,9 @@ int snd_soc_card_of_parse_audio_routing(struct snd_soc_card *card, const char *p
 int snd_soc_card_of_parse_aux_devs(struct snd_soc_card *card, const char *propname);
 int snd_soc_card_of_parse_ignore_suspend_widgets(struct snd_soc_card *card, const char *propname);
 
+int snd_soc_card_fixup_dai_links_platform_name(struct snd_soc_card *card,
+					       const char *platform_name);
+
 #ifdef CONFIG_PCI
 void snd_soc_card_set_pci_ssid(struct snd_soc_card *card,
 			       unsigned short vendor,
