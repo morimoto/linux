51c81180ff96180048ea5413c3b313c2d62ed3e5
Marek Vasut <marek.vasut+renesas@mailbox.org>
2026-07-04 14:54:42 +0200

    Revert "net: ethernet: renesas: rcar_gen4_ptp: Remove different memory layout"
    This reverts commit 4da47931a9240ff44547d30983cb23e26b05d180.

--- drivers/net/ethernet/renesas/rcar_gen4_ptp.c
+++ drivers/net/ethernet/renesas/rcar_gen4_ptp.c
@@ -130,8 +130,12 @@ static struct ptp_clock_info rcar_gen4_ptp_info = {
 	.enable = rcar_gen4_ptp_enable,
 };
 
-static int rcar_gen4_ptp_set_offs(struct rcar_gen4_ptp_private *ptp_priv)
+static int rcar_gen4_ptp_set_offs(struct rcar_gen4_ptp_private *ptp_priv,
+				  enum rcar_gen4_ptp_reg_layout layout)
 {
+	if (layout != RCAR_GEN4_PTP_REG_LAYOUT)
+		return -EINVAL;
+
 	ptp_priv->offs = &gen4_offs;
 
 	return 0;
@@ -147,7 +151,8 @@ static s64 rcar_gen4_ptp_rate_to_increment(u32 rate)
 	return div_s64(1000000000LL << 27, rate);
 }
 
-int rcar_gen4_ptp_register(struct rcar_gen4_ptp_private *ptp_priv, u32 rate)
+int rcar_gen4_ptp_register(struct rcar_gen4_ptp_private *ptp_priv,
+			   enum rcar_gen4_ptp_reg_layout layout, u32 rate)
 {
 	int ret;
 
@@ -156,7 +161,7 @@ int rcar_gen4_ptp_register(struct rcar_gen4_ptp_private *ptp_priv, u32 rate)
 
 	spin_lock_init(&ptp_priv->lock);
 
-	ret = rcar_gen4_ptp_set_offs(ptp_priv);
+	ret = rcar_gen4_ptp_set_offs(ptp_priv, layout);
 	if (ret)
 		return ret;
 
