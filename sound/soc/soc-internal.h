d37085a701f74583e8e34387de21d936c6c7a2b9
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-07-07 11:43:30 +0900

    ASoC: soc-component: add snd_soc_component_connect_dai()
    struct snd_soc_component will be capsuled soon, its member will not be
    able to access from non soc-component.c.
    
    This patch adds snd_soc_component_connect_dai() to connect DAI to
    Component.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/soc-internal.h
+++ sound/soc/soc-internal.h
@@ -58,6 +58,7 @@ void snd_soc_component_remove(struct snd_soc_component *component, int probed);
 int snd_soc_component_probe(struct snd_soc_card *card, struct snd_soc_component *component);
 struct list_head *snd_soc_component_get_list_head(void);
 void snd_soc_component_of_put(struct snd_soc_dai_link_component *component);
+void snd_soc_component_connect_dai(struct snd_soc_component *component, struct snd_soc_dai *dai);
 
 #define for_each_component(component)					\
 	list_for_each_entry(component, snd_soc_component_get_list_head(), list)
