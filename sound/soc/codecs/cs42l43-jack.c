bfddd196dc3225b668037f13ce56dccac7fd64f9
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-07-30 15:51:53 +0900

    header cleanup
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/codecs/cs42l43-jack.c
+++ sound/soc/codecs/cs42l43-jack.c
@@ -14,18 +14,10 @@
 #include <linux/jiffies.h>
 #include <linux/mfd/cs42l43.h>
 #include <linux/mfd/cs42l43-regs.h>
-#include <linux/mutex.h>
 #include <linux/pm_runtime.h>
 #include <linux/property.h>
-#include <linux/regmap.h>
 #include <linux/time.h>
 #include <linux/workqueue.h>
-#include <sound/control.h>
-#include <sound/jack.h>
-#include <sound/pcm.h>
-#include <sound/pcm_params.h>
-#include <sound/soc-component.h>
-#include <sound/soc-jack.h>
 #include <sound/soc.h>
 
 #include "cs42l43.h"
