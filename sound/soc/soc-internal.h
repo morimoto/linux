2e04a99a4d9fe4f340f70d5d5f98cb7acf8f2ffd
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-08-07 08:44:38 +0900

    ASoC: soc-component: move snd_soc_component_set_aux() to soc-internal.h
    It is used from ASoC framework internal only. Move it to soc-internal.h.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/soc-internal.h
+++ sound/soc/soc-internal.h
@@ -139,6 +139,7 @@ void snd_soc_component_connect_dai(struct snd_soc_component *component, struct s
 void snd_soc_component_active_action(struct snd_soc_component *component, int action);
 void snd_soc_component_device_link_del(struct snd_soc_component *component);
 int snd_soc_component_device_link_add(struct snd_soc_component *component);
+void snd_soc_component_set_aux(struct snd_soc_component *component, struct snd_soc_aux_dev *aux);
 
 #define for_each_component(component)							\
 	for (component = snd_soc_component_from_component_total_list(snd_soc_component_total_list_head()->next);	\
