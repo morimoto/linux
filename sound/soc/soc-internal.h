00f3347124ba9ad9a866864c8351b70dd54e3806
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-08-07 08:18:09 +0900

    ASoC: soc-dai: move snd_soc_dai_mute_is_ctrled_at_trigger() to soc-internal.h
    It is used from ASoC framework internal only. Move it to soc-internal.h.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/soc-internal.h
+++ sound/soc/soc-internal.h
@@ -87,6 +87,7 @@ void snd_soc_dai_active_action(struct snd_soc_dai *dai, int stream, int action);
 void snd_soc_dai_get_bclk(struct snd_soc_dai *dai, struct clk **bclk, unsigned int *bclk_ratio);
 unsigned int snd_soc_dai_auto_select_format(const struct snd_soc_pcm_runtime *rtd);
 int snd_soc_dai_prepare(struct snd_soc_dai *dai, struct snd_pcm_substream *substream);
+int snd_soc_dai_mute_is_ctrled_at_trigger(struct snd_soc_dai *dai);
 
 /*
  * In soc-component
