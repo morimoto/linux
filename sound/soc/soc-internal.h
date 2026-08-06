2e3e0874c7a7337f7bd8d63239122bba820e9259
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-08-06 13:47:55 +0900

    ASoC: soc-component: rename list to component_total_list
    Current Component is using "list" to connecting to "component_list".
    
    It is unclear and confusable naming. Let's rename it as
    "component_total_list" and "component_total_list_head".
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/soc-internal.h
+++ sound/soc/soc-internal.h
@@ -56,7 +56,7 @@ bool snd_soc_component_matches_dlc(struct snd_soc_component *component,
 				   const struct snd_soc_dai_link_component *dlc);
 void snd_soc_component_remove(struct snd_soc_component *component, int probed);
 int snd_soc_component_probe(struct snd_soc_card *card, struct snd_soc_component *component);
-struct list_head *snd_soc_component_get_list_head(void);
+struct list_head *snd_soc_component_total_list_head(void);
 void snd_soc_component_of_put(struct snd_soc_dai_link_component *component);
 void snd_soc_component_connect_dai(struct snd_soc_component *component, struct snd_soc_dai *dai);
 void snd_soc_component_active_action(struct snd_soc_component *component, int action);
@@ -64,6 +64,6 @@ void snd_soc_component_device_link_del(struct snd_soc_component *component);
 int snd_soc_component_device_link_add(struct snd_soc_component *component);
 
 #define for_each_component(component)					\
-	list_for_each_entry(component, snd_soc_component_get_list_head(), list)
+	list_for_each_entry(component, snd_soc_component_total_list_head(), component_total_list)
 
 #endif /* __SOC_INTERNAL_H */
