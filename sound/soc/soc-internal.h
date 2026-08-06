f0f3f10b9f9beed755203ac7dbb4a5ce453e3616
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-08-07 08:16:48 +0900

    ASoC: soc-dai: move snd_soc_dai_prepare() to soc-internal.h
    It is used from ASoC framework internal only. Move it to soc-internal.h.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/soc-internal.h
+++ sound/soc/soc-internal.h
@@ -86,6 +86,7 @@ struct clk *snd_soc_dai_get_bclk(struct snd_soc_dai *dai);
 unsigned int snd_soc_dai_get_bclk_ratio(struct snd_soc_dai *dai);
 void snd_soc_dai_active_update(struct snd_soc_dai *dai, int stream, int action);
 unsigned int snd_soc_dai_auto_select_format(const struct snd_soc_pcm_runtime *rtd);
+int snd_soc_dai_prepare(struct snd_soc_dai *dai, struct snd_pcm_substream *substream);
 
 /*
  * In soc-component
