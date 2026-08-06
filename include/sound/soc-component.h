f8af4c65dd1ff638e8cab80cdd9920a18d76e566
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-08-07 08:44:38 +0900

    ASoC: soc-component: move snd_soc_component_set_aux() to soc-internal.h
    It is used from ASoC framework internal only. Move it to soc-internal.h.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- include/sound/soc-component.h
+++ include/sound/soc-component.h
@@ -247,8 +247,6 @@ const char *snd_soc_component_name_prefix(struct snd_soc_component *component);
 void snd_soc_component_set_priv(struct snd_soc_component *component, void *priv);
 void *snd_soc_component_to_priv(struct snd_soc_component *component);
 
-void snd_soc_component_set_aux(struct snd_soc_component *component,
-			       struct snd_soc_aux_dev *aux);
 int snd_soc_component_init(struct snd_soc_component *component);
 int snd_soc_component_is_dummy(struct snd_soc_component *component);
 
