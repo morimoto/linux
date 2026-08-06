c0c1646d695a33b0d84004651649301816267b38
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-08-07 08:37:03 +0900

    ASoC: soc-dai: move snd_soc_dai_matches_xxx() to soc-internal.h
    It is used from ASoC framework internal only. Move it to soc-internal.h.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/soc-internal.h
+++ sound/soc/soc-internal.h
@@ -120,6 +120,8 @@ int snd_soc_dai_compr_set_metadata(struct snd_soc_dai *dai, struct snd_compr_str
 				   struct snd_compr_metadata *metadata);
 int snd_soc_dai_compr_get_metadata(struct snd_soc_dai *dai, struct snd_compr_stream *cstream,
 				   struct snd_compr_metadata *metadata);
+int snd_soc_dai_matches_args(const struct snd_soc_dai *dai, const struct of_phandle_args *args2);
+int snd_soc_dai_matches_dlc(struct snd_soc_dai *dai, const struct snd_soc_dai_link_component *dlc);
 
 /*
  * In soc-component
