0141a31af40e6df720608edf0a94cb4d247a9554
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-09-02 10:44:20 +0900

    ASoC: soc-card: add snd_soc_card_rebind()
    struct snd_soc_card will be capsuled soon, its member will not be
    able to access from non soc-card.c.
    
    add snd_soc_card_rebind() and capsuled card relarted operation
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/soc-internal.h
+++ sound/soc/soc-internal.h
@@ -55,6 +55,7 @@ void snd_soc_dapm_suspend_resume(struct snd_soc_card *card, int event);
  * In soc-card
  */
 void snd_soc_card_unbind(struct snd_soc_card *card);
+void snd_soc_card_rebind(void);
 int snd_soc_card_bind_call(struct snd_soc_card *card);
 
 /*
