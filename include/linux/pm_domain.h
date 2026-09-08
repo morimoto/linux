kernelorg-mirror/linux_kernel_git_geert_renesas-drivers

pmdomain: Make genpd_get_from_provider() public
Unlike the corresponding function in the clock subsystem
(of_clk_get_from_provider()), genpd_get_from_provider() is private, and
thus cannot be used by PM Domain drivers.

Make it public, so it be used by the R-Car X5H Module Controller driver.

Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

include/linux/pm_domain.h
@@ -458,6 +458,7 @@ int of_genpd_add_provider_simple(struct device_node *np,
 int of_genpd_add_provider_onecell(struct device_node *np,
 				  struct genpd_onecell_data *data);
 void of_genpd_del_provider(struct device_node *np);
+struct generic_pm_domain *genpd_get_from_provider(const struct of_phandle_args *genpdspec);
 int of_genpd_add_device(const struct of_phandle_args *args, struct device *dev);
 int of_genpd_add_subdomain(const struct of_phandle_args *parent_spec,
 			   const struct of_phandle_args *subdomain_spec);
@@ -487,6 +488,12 @@ static inline int of_genpd_add_provider_onecell(struct device_node *np,
 }
 
 static inline void of_genpd_del_provider(struct device_node *np) {}
+
+static inline struct generic_pm_domain *genpd_get_from_provider(
+				const struct of_phandle_args *genpdspec)
+{
+	return ERR_PTR(-ENODEV);
+}
 
 static inline int of_genpd_add_device(const struct of_phandle_args *args,
 				      struct device *dev)

morimoto/linux

Add an "of_"-prefix for consistency with public genpd function naming.
Link: https://patch.msgid.link/1e1b20976fb2b0b78684be315cdf78960efcc403.1788338320.git.geert+renesas@glider.be
[backport: Drop changes to not yet existing functions]

include/linux/pm_domain.h
@@ -456,6 +456,7 @@ int of_genpd_add_provider_simple(struct device_node *np,
 int of_genpd_add_provider_onecell(struct device_node *np,
 				  struct genpd_onecell_data *data);
 void of_genpd_del_provider(struct device_node *np);
+struct generic_pm_domain *of_genpd_get_from_provider(const struct of_phandle_args *genpdspec);
 int of_genpd_add_device(const struct of_phandle_args *args, struct device *dev);
 int of_genpd_add_subdomain(const struct of_phandle_args *parent_spec,
 			   const struct of_phandle_args *subdomain_spec);
@@ -485,6 +486,12 @@ static inline int of_genpd_add_provider_onecell(struct device_node *np,
 }
 
 static inline void of_genpd_del_provider(struct device_node *np) {}
+
+static inline struct generic_pm_domain *of_genpd_get_from_provider(
+				const struct of_phandle_args *genpdspec)
+{
+	return ERR_PTR(-ENODEV);
+}
 
 static inline int of_genpd_add_device(const struct of_phandle_args *args,
 				      struct device *dev)