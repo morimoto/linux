kernelorg-mirror/linux_kernel_git_geert_renesas-drivers

pmdomain: renesas: r8a78000: Add support for SCP FW SDKv4.28.0
Add preliminary power domain and module mapping tables for SCP FW
SDKv4.28.0.

Suggested-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
---
v2:
  - Spin off from "pmdomain: renesas: Add R-Car X5H MDLC SCMI remapping
    driver".

drivers/pmdomain/renesas/r8a78000-mdlc.c
@@ -585,7 +585,244 @@ static const struct mdlc_info r8a78000_mdlc_default[] = {
 	{ 0 }
 };
 
+// FIXME We don't need all of them from the start; only add when used/tested
+static const struct power_map_in r8a78000_mdlc_pere_power_fw_4_28_0[] = {
+	{ 0, 12 },	/* PD_UFS0 */
+	{ 1, 13 },	/* PD_UFS1 */
+	{ -1 },
+};
+
+static const struct mod_map r8a78000_mdlc_pere_mod_fw_4_28_0[] = {
+	{ 0x30, 197 },	/* PERE_GPIODM0 */
+	// No CLOCK_ATTRIBUTES { 0x31, 198 },	/* PERE_GPIODM1 */
+	// No CLOCK_ATTRIBUTES { 0x32, 199 },	/* PERE_GPIODM2 */
+	// No CLOCK_ATTRIBUTES { 0x33, 200 },	/* PERE_GPIODM3 */
+	{ 0x40, 201 },	/* RPC */
+	{ 0x60, 202 },	/* UFS0 */
+	{ 0x61, 203 },	/* UFS1 */
+	{ 0x70, 204 },	/* SDHI0 */
+	{ -1 },
+};
+
+static const struct mod_map r8a78000_mdlc_perw_mod_fw_4_28_0[] = {
+	{ 0x30, 205 },	/* PERW_GPIODM0 */
+	// No CLOCK_ATTRIBUTES { 0x31, 206 },	/* PERW_GPIODM1 */
+	// No CLOCK_ATTRIBUTES { 0x32, 207 },	/* PERW_GPIODM2 */
+	// No CLOCK_ATTRIBUTES { 0x33, 208 },	/* PERW_GPIODM3 */
+	{ 0x40, 209 },	/* SCIF0 */
+	{ 0x41, 210 },	/* SCIF1 */
+	{ 0x42, 211 },	/* SCIF3 */
+	{ 0x43, 212 },	/* SCIF4 */
+	{ 0x44, 213 },	/* I2C1 */
+	{ 0x45, 214 },	/* I2C2 */
+	{ 0x46, 215 },	/* I2C3 */
+	{ 0x47, 216 },	/* I2C4 */
+	{ 0x48, 217 },	/* I2C5 */
+	{ 0x49, 218 },	/* I2C6 */
+	{ 0x4a, 219 },	/* I2C7 */
+	{ 0x4b, 220 },	/* I2C8 */
+	{ 0x4c, 221 },	/* I3C0 */
+	{ 0x4d, 222 },	/* I3C1 */
+	{ 0x4e, 223 },	/* I3C2 */
+	{ 0x4f, 224 },	/* MSI4 */
+	{ 0x50, 225 },	/* MSI5 */
+	{ 0x51, 226 },	/* MSI6 */
+	{ 0x52, 227 },	/* MSI7 */
+	/*
+	 * HSCIF0 is protected:
+	 *   - CLOCK_ATTRIBUTES is not supported, so clk is NULL
+	 *   - Reset operations fail with -EOPNOTSUPP
+	 */
+	{ 0x54, 228 },	/* HSCIF0 */
+	{ 0x55, 229 },	/* HSCIF1 */
+	{ 0x56, 230 },	/* HSCIF2 */
+	{ 0x57, 231 },	/* HSCIF3 */
+	{ 0x58, 232 },	/* DRI00 */
+	{ 0x59, 233 },	/* DRI01 */
+	{ 0x5a, 234 },	/* DRI10 */
+	{ 0x5b, 235 },	/* DRI11 */
+	{ 0x5c, 236 },	/* DRI20 */
+	{ 0x5d, 237 },	/* DRI21 */
+	{ 0x5e, 238 },	/* DRI30 */
+	{ 0x5f, 239 },	/* DRI31 */
+	{ 0x60, 240 },	/* DRI40 */
+	{ 0x61, 241 },	/* DRI41 */
+	{ 0x62, 242 },	/* DRI50 */
+	{ 0x63, 243 },	/* DRI51 */
+	{ 0x64, 244 },	/* DRI60 */
+	{ 0x65, 245 },	/* DRI61 */
+	{ 0x66, 246 },	/* DRI70 */
+	{ 0x67, 247 },	/* DRI71 */
+	{ 0x70, 248 },	/* PWM0 */
+	{ 0x72, 249 },	/* TMU1 */
+	{ 0x73, 250 },	/* TMU2 */
+	{ 0x74, 251 },	/* TMU3 */
+	{ 0x75, 252 },	/* TMU4 */
+	{ 0x76, 253 },	/* TPU0 */
+	{ 0x90, 254 },	/* ADG0 */
+	{ 0x91, 255 },	/* ADG1 */
+	{ 0x92, 256 },	/* SSI0 */
+	{ 0x93, 257 },	/* SSI00 */
+	{ 0x94, 258 },	/* SSI01 */
+	{ 0x95, 259 },	/* SSI02 */
+	{ 0x96, 260 },	/* SSI03 */
+	{ 0x97, 261 },	/* SSI04 */
+	{ 0x98, 262 },	/* SSI05 */
+	{ 0x99, 263 },	/* SSI06 */
+	{ 0x9a, 264 },	/* SSI07 */
+	{ 0x9b, 265 },	/* SSI08 */
+	{ 0x9c, 266 },	/* SSI09 */
+	{ 0x9d, 267 },	/* SSI1 */
+	{ 0x9e, 268 },	/* SSI10 */
+	{ 0x9f, 269 },	/* SSI11 */
+	{ 0xa0, 270 },	/* SSI12 */
+	{ 0xa1, 271 },	/* SSI13 */
+	{ 0xa2, 272 },	/* SSI14 */
+	{ 0xa3, 273 },	/* SSI15 */
+	{ 0xa4, 274 },	/* SSI16 */
+	{ 0xa5, 275 },	/* SSI17 */
+	{ 0xa6, 276 },	/* SSI18 */
+	{ 0xa7, 277 },	/* SSI19 */
+	{ 0xa8, 278 },	/* SCU0 */
+	{ 0xa9, 279 },	/* SRC00 */
+	{ 0xaa, 280 },	/* SRC01 */
+	{ 0xab, 281 },	/* SRC02 */
+	{ 0xac, 282 },	/* SRC03 */
+	{ 0xad, 283 },	/* SRC04 */
+	{ 0xae, 284 },	/* SRC05 */
+	{ 0xaf, 285 },	/* SRC06 */
+	{ 0xb0, 286 },	/* SRC07 */
+	{ 0xb1, 287 },	/* SRC08 */
+	{ 0xb2, 288 },	/* SRC09 */
+	{ 0xb3, 289 },	/* SCU00 */
+	{ 0xb4, 290 },	/* SCU01 */
+	{ 0xb5, 291 },	/* DVC00 */
+	{ 0xb6, 292 },	/* DVC01 */
+	{ 0xb7, 293 },	/* SCU1 */
+	{ 0xb8, 294 },	/* SRC10 */
+	{ 0xb9, 295 },	/* SRC11 */
+	{ 0xba, 296 },	/* SRC12 */
+	{ 0xbb, 297 },	/* SRC13 */
+	{ 0xbc, 298 },	/* SRC14 */
+	{ 0xbd, 299 },	/* SRC15 */
+	{ 0xbe, 300 },	/* SRC16 */
+	{ 0xbf, 301 },	/* SRC17 */
+	{ 0xc0, 302 },	/* SRC18 */
+	{ 0xc1, 303 },	/* SRC19 */
+	{ 0xc2, 304 },	/* SCU10 */
+	{ 0xc3, 305 },	/* SCU11 */
+	{ 0xc4, 306 },	/* DVC10 */
+	{ 0xc5, 307 },	/* DVC11 */
+	{ 0xc6, 308 },	/* APD00 */
+	{ 0xc7, 309 },	/* APD01 */
+	{ 0xc8, 310 },	/* APD10 */
+	{ 0xc9, 311 },	/* APD11 */
+	{ 0xca, 312 },	/* APD02 */
+	{ 0xcb, 313 },	/* APD12 */
+	{ -1 },
+};
+
+static const struct mdlc_info r8a78000_mdlc_fw_4_28_0[] = {
+	{
+		.base = 0xc3060000 /* mdlc_vipn */,
+		/* FIXME .power_map = r8a78000_mdlc_vipn_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_vipn_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc3460000 /* mdlc_vips */,
+		/* FIXME .power_map = r8a78000_mdlc_vips_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_vips_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc5000000 /* mdlc_vio */,
+		/* FIXME .power_map = r8a78000_mdlc_vio_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_vio_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc08f0000 /* mdlc_pere */,
+		.power_map = r8a78000_mdlc_pere_power_fw_4_28_0,
+		.mod_map = r8a78000_mdlc_pere_mod_fw_4_28_0,
+	}, {
+		.base = 0xc05d0000 /* mdlc_perw */,
+		.mod_map = r8a78000_mdlc_perw_mod_fw_4_28_0,
+// FIXME We don't need all of them from the start; only add when used/tested
+#if 0
+	}, {
+		.base = 0xe8000000 /* mdlc_ddr0 */,
+	}, {
+		.base = 0xe8080000 /* mdlc_ddr1 */,
+	}, {
+		.base = 0xe8100000 /* mdlc_ddr2 */,
+	}, {
+		.base = 0xe8180000 /* mdlc_ddr3 */,
+	}, {
+		.base = 0xe8200000 /* mdlc_ddr4 */,
+	}, {
+		.base = 0xe8280000 /* mdlc_ddr5 */,
+	}, {
+		.base = 0xe8300000 /* mdlc_ddr6 */,
+	}, {
+		.base = 0xe8380000 /* mdlc_ddr7 */,
+	}, {
+		.base = 0xc9c90000 /* mdlc_hscn */,
+		/* FIXME .power_map = r8a78000_mdlc_hscn_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_hscn_mod_fw_4_28_0, */
+	}, {
+		.base = 0x19440000 /* mdlc_rt */,
+		/* FIXME .power_map = r8a78000_mdlc_rt_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_rt_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc6480000 /* mdlc_top */,
+		/* FIXME .mod_map = r8a78000_mdlc_top_mod_fw_4_28_0, */
+	}, {
+		.base = 0xde200000 /* mdlc_hscs */,
+		/* FIXME .power_map = r8a78000_mdlc_hscs_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_hscs_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc1990000 /* mdlc_imn */,
+		/* FIXME .power_map = r8a78000_mdlc_imn_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_imn_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc1d90000 /* mdlc_ims */,
+		/* FIXME .power_map = r8a78000_mdlc_ims_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_ims_mod_fw_4_28_0, */
+	}, {
+		.base = 0xcb510000 /* mdlc_gpc */,
+		/* FIXME .power_map = r8a78000_mdlc_gpc_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_gpc_mod_fw_4_28_0, */
+	}, {
+		.base = 0xcbe90000 /* mdlc_dsp */,
+		/* FIXME .power_map = r8a78000_mdlc_dsp_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_dsp_mod_fw_4_28_0, */
+	}, {
+		.base = 0xe9980000 /* mdlc_mm */,
+		/* FIXME .mod_map = r8a78000_mdlc_mm_mod_fw_4_28_0, */
+	}, {
+		.base = 0xd2c30000 /* mdlc_npu0 */,
+		/* FIXME .power_map = r8a78000_mdlc_npu0_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_npu0_mod_fw_4_28_0, */
+	}, {
+		.base = 0xd6c30000 /* mdlc_npu1 */,
+		/* FIXME .power_map = r8a78000_mdlc_npu1_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_npu1_mod_fw_4_28_0, */
+	}, {
+		.base = 0xca410000 /* mdlc_cmnn */,
+		/* FIXME .power_map = r8a78000_mdlc_cmnn_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_cmnn_mod_fw_4_28_0, */
+	}, {
+		.base = 0xca510000 /* mdlc_cmns */,
+		/* FIXME .power_map = r8a78000_mdlc_cmns_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_cmns_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc1330000 /* mdlc_scp */,
+		/* FIXME .mod_map = r8a78000_mdlc_scp_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc1338000 /* mdlc_aon */,
+		/* FIXME .mod_map = r8a78000_mdlc_aon_mod_fw_4_28_0, */
+#endif
+	},
+	{ 0 }
+};
+
 static const struct fw_map r8a78000_mdlc_fw_map[] = {
+	{ 0x010a0000, r8a78000_mdlc_fw_4_28_0 },	/* SCP FW SDKv4.28.0 */
 	{ 0, NULL }
 };
 

morimoto/linux

drivers/pmdomain/renesas/r8a78000-mdlc.c
@@ -624,7 +624,244 @@ static const struct mdlc_info r8a78000_mdlc_default[] = {
 	{ /* sentinel */ }
 };
 
+// FIXME We don't need all of them from the start; only add when used/tested
+static const struct power_map_in r8a78000_mdlc_pere_power_fw_4_28_0[] = {
+	{ 0, 12 },	/* PD_UFS0 */
+	{ 1, 13 },	/* PD_UFS1 */
+	{ -1 }
+};
+
+static const struct mod_map_in r8a78000_mdlc_pere_mod_fw_4_28_0[] = {
+	{ 0x30, 197 },	/* PERE_GPIODM0 */
+	// No CLOCK_ATTRIBUTES { 0x31, 198 },	/* PERE_GPIODM1 */
+	// No CLOCK_ATTRIBUTES { 0x32, 199 },	/* PERE_GPIODM2 */
+	// No CLOCK_ATTRIBUTES { 0x33, 200 },	/* PERE_GPIODM3 */
+	{ 0x40, 201 },	/* RPC */
+	{ 0x60, 202 },	/* UFS0 */
+	{ 0x61, 203 },	/* UFS1 */
+	{ 0x70, 204 },	/* SDHI0 */
+	{ -1 }
+};
+
+static const struct mod_map_in r8a78000_mdlc_perw_mod_fw_4_28_0[] = {
+	{ 0x30, 205 },	/* PERW_GPIODM0 */
+	// No CLOCK_ATTRIBUTES { 0x31, 206 },	/* PERW_GPIODM1 */
+	// No CLOCK_ATTRIBUTES { 0x32, 207 },	/* PERW_GPIODM2 */
+	// No CLOCK_ATTRIBUTES { 0x33, 208 },	/* PERW_GPIODM3 */
+	{ 0x40, 209 },	/* SCIF0 */
+	{ 0x41, 210 },	/* SCIF1 */
+	{ 0x42, 211 },	/* SCIF3 */
+	{ 0x43, 212 },	/* SCIF4 */
+	{ 0x44, 213 },	/* I2C1 */
+	{ 0x45, 214 },	/* I2C2 */
+	{ 0x46, 215 },	/* I2C3 */
+	{ 0x47, 216 },	/* I2C4 */
+	{ 0x48, 217 },	/* I2C5 */
+	{ 0x49, 218 },	/* I2C6 */
+	{ 0x4a, 219 },	/* I2C7 */
+	{ 0x4b, 220 },	/* I2C8 */
+	{ 0x4c, 221 },	/* I3C0 */
+	{ 0x4d, 222 },	/* I3C1 */
+	{ 0x4e, 223 },	/* I3C2 */
+	{ 0x4f, 224 },	/* MSI4 */
+	{ 0x50, 225 },	/* MSI5 */
+	{ 0x51, 226 },	/* MSI6 */
+	{ 0x52, 227 },	/* MSI7 */
+	/*
+	 * HSCIF0 is protected:
+	 *   - CLOCK_ATTRIBUTES is not supported, so clk is NULL
+	 *   - Reset operations fail with -EOPNOTSUPP
+	 */
+	{ 0x54, 228 },	/* HSCIF0 */
+	{ 0x55, 229 },	/* HSCIF1 */
+	{ 0x56, 230 },	/* HSCIF2 */
+	{ 0x57, 231 },	/* HSCIF3 */
+	{ 0x58, 232 },	/* DRI00 */
+	{ 0x59, 233 },	/* DRI01 */
+	{ 0x5a, 234 },	/* DRI10 */
+	{ 0x5b, 235 },	/* DRI11 */
+	{ 0x5c, 236 },	/* DRI20 */
+	{ 0x5d, 237 },	/* DRI21 */
+	{ 0x5e, 238 },	/* DRI30 */
+	{ 0x5f, 239 },	/* DRI31 */
+	{ 0x60, 240 },	/* DRI40 */
+	{ 0x61, 241 },	/* DRI41 */
+	{ 0x62, 242 },	/* DRI50 */
+	{ 0x63, 243 },	/* DRI51 */
+	{ 0x64, 244 },	/* DRI60 */
+	{ 0x65, 245 },	/* DRI61 */
+	{ 0x66, 246 },	/* DRI70 */
+	{ 0x67, 247 },	/* DRI71 */
+	{ 0x70, 248 },	/* PWM0 */
+	{ 0x72, 249 },	/* TMU1 */
+	{ 0x73, 250 },	/* TMU2 */
+	{ 0x74, 251 },	/* TMU3 */
+	{ 0x75, 252 },	/* TMU4 */
+	{ 0x76, 253 },	/* TPU0 */
+	{ 0x90, 254 },	/* ADG0 */
+	{ 0x91, 255 },	/* ADG1 */
+	{ 0x92, 256 },	/* SSI0 */
+	{ 0x93, 257 },	/* SSI00 */
+	{ 0x94, 258 },	/* SSI01 */
+	{ 0x95, 259 },	/* SSI02 */
+	{ 0x96, 260 },	/* SSI03 */
+	{ 0x97, 261 },	/* SSI04 */
+	{ 0x98, 262 },	/* SSI05 */
+	{ 0x99, 263 },	/* SSI06 */
+	{ 0x9a, 264 },	/* SSI07 */
+	{ 0x9b, 265 },	/* SSI08 */
+	{ 0x9c, 266 },	/* SSI09 */
+	{ 0x9d, 267 },	/* SSI1 */
+	{ 0x9e, 268 },	/* SSI10 */
+	{ 0x9f, 269 },	/* SSI11 */
+	{ 0xa0, 270 },	/* SSI12 */
+	{ 0xa1, 271 },	/* SSI13 */
+	{ 0xa2, 272 },	/* SSI14 */
+	{ 0xa3, 273 },	/* SSI15 */
+	{ 0xa4, 274 },	/* SSI16 */
+	{ 0xa5, 275 },	/* SSI17 */
+	{ 0xa6, 276 },	/* SSI18 */
+	{ 0xa7, 277 },	/* SSI19 */
+	{ 0xa8, 278 },	/* SCU0 */
+	{ 0xa9, 279 },	/* SRC00 */
+	{ 0xaa, 280 },	/* SRC01 */
+	{ 0xab, 281 },	/* SRC02 */
+	{ 0xac, 282 },	/* SRC03 */
+	{ 0xad, 283 },	/* SRC04 */
+	{ 0xae, 284 },	/* SRC05 */
+	{ 0xaf, 285 },	/* SRC06 */
+	{ 0xb0, 286 },	/* SRC07 */
+	{ 0xb1, 287 },	/* SRC08 */
+	{ 0xb2, 288 },	/* SRC09 */
+	{ 0xb3, 289 },	/* SCU00 */
+	{ 0xb4, 290 },	/* SCU01 */
+	{ 0xb5, 291 },	/* DVC00 */
+	{ 0xb6, 292 },	/* DVC01 */
+	{ 0xb7, 293 },	/* SCU1 */
+	{ 0xb8, 294 },	/* SRC10 */
+	{ 0xb9, 295 },	/* SRC11 */
+	{ 0xba, 296 },	/* SRC12 */
+	{ 0xbb, 297 },	/* SRC13 */
+	{ 0xbc, 298 },	/* SRC14 */
+	{ 0xbd, 299 },	/* SRC15 */
+	{ 0xbe, 300 },	/* SRC16 */
+	{ 0xbf, 301 },	/* SRC17 */
+	{ 0xc0, 302 },	/* SRC18 */
+	{ 0xc1, 303 },	/* SRC19 */
+	{ 0xc2, 304 },	/* SCU10 */
+	{ 0xc3, 305 },	/* SCU11 */
+	{ 0xc4, 306 },	/* DVC10 */
+	{ 0xc5, 307 },	/* DVC11 */
+	{ 0xc6, 308 },	/* APD00 */
+	{ 0xc7, 309 },	/* APD01 */
+	{ 0xc8, 310 },	/* APD10 */
+	{ 0xc9, 311 },	/* APD11 */
+	{ 0xca, 312 },	/* APD02 */
+	{ 0xcb, 313 },	/* APD12 */
+	{ -1 }
+};
+
+static const struct mdlc_info r8a78000_mdlc_fw_4_28_0[] = {
+	{
+		.base = 0xc3060000 /* mdlc_vipn */,
+		/* FIXME .power_map = r8a78000_mdlc_vipn_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_vipn_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc3460000 /* mdlc_vips */,
+		/* FIXME .power_map = r8a78000_mdlc_vips_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_vips_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc5000000 /* mdlc_vio */,
+		/* FIXME .power_map = r8a78000_mdlc_vio_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_vio_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc08f0000 /* mdlc_pere */,
+		.power_map = r8a78000_mdlc_pere_power_fw_4_28_0,
+		.mod_map = r8a78000_mdlc_pere_mod_fw_4_28_0,
+	}, {
+		.base = 0xc05d0000 /* mdlc_perw */,
+		.mod_map = r8a78000_mdlc_perw_mod_fw_4_28_0,
+// FIXME We don't need all of them from the start; only add when used/tested
+#if 0
+	}, {
+		.base = 0xe8000000 /* mdlc_ddr0 */,
+	}, {
+		.base = 0xe8080000 /* mdlc_ddr1 */,
+	}, {
+		.base = 0xe8100000 /* mdlc_ddr2 */,
+	}, {
+		.base = 0xe8180000 /* mdlc_ddr3 */,
+	}, {
+		.base = 0xe8200000 /* mdlc_ddr4 */,
+	}, {
+		.base = 0xe8280000 /* mdlc_ddr5 */,
+	}, {
+		.base = 0xe8300000 /* mdlc_ddr6 */,
+	}, {
+		.base = 0xe8380000 /* mdlc_ddr7 */,
+	}, {
+		.base = 0xc9c90000 /* mdlc_hscn */,
+		/* FIXME .power_map = r8a78000_mdlc_hscn_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_hscn_mod_fw_4_28_0, */
+	}, {
+		.base = 0x19440000 /* mdlc_rt */,
+		/* FIXME .power_map = r8a78000_mdlc_rt_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_rt_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc6480000 /* mdlc_top */,
+		/* FIXME .mod_map = r8a78000_mdlc_top_mod_fw_4_28_0, */
+	}, {
+		.base = 0xde200000 /* mdlc_hscs */,
+		/* FIXME .power_map = r8a78000_mdlc_hscs_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_hscs_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc1990000 /* mdlc_imn */,
+		/* FIXME .power_map = r8a78000_mdlc_imn_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_imn_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc1d90000 /* mdlc_ims */,
+		/* FIXME .power_map = r8a78000_mdlc_ims_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_ims_mod_fw_4_28_0, */
+	}, {
+		.base = 0xcb510000 /* mdlc_gpc */,
+		/* FIXME .power_map = r8a78000_mdlc_gpc_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_gpc_mod_fw_4_28_0, */
+	}, {
+		.base = 0xcbe90000 /* mdlc_dsp */,
+		/* FIXME .power_map = r8a78000_mdlc_dsp_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_dsp_mod_fw_4_28_0, */
+	}, {
+		.base = 0xe9980000 /* mdlc_mm */,
+		/* FIXME .mod_map = r8a78000_mdlc_mm_mod_fw_4_28_0, */
+	}, {
+		.base = 0xd2c30000 /* mdlc_npu0 */,
+		/* FIXME .power_map = r8a78000_mdlc_npu0_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_npu0_mod_fw_4_28_0, */
+	}, {
+		.base = 0xd6c30000 /* mdlc_npu1 */,
+		/* FIXME .power_map = r8a78000_mdlc_npu1_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_npu1_mod_fw_4_28_0, */
+	}, {
+		.base = 0xca410000 /* mdlc_cmnn */,
+		/* FIXME .power_map = r8a78000_mdlc_cmnn_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_cmnn_mod_fw_4_28_0, */
+	}, {
+		.base = 0xca510000 /* mdlc_cmns */,
+		/* FIXME .power_map = r8a78000_mdlc_cmns_power_fw_4_28_0, */
+		/* FIXME .mod_map = r8a78000_mdlc_cmns_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc1330000 /* mdlc_scp */,
+		/* FIXME .mod_map = r8a78000_mdlc_scp_mod_fw_4_28_0, */
+	}, {
+		.base = 0xc1338000 /* mdlc_aon */,
+		/* FIXME .mod_map = r8a78000_mdlc_aon_mod_fw_4_28_0, */
+#endif
+	},
+	{ /* sentinel */ }
+};
+
 static const struct fw_map r8a78000_mdlc_fw_map[] = {
+	{ 0x010a0000, r8a78000_mdlc_fw_4_28_0 },	/* SCP FW SDKv4.28.0 */
 	{ /* sentinel */ }
 };
 